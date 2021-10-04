6a46648606687b2384807e627cb3ea74042e83f2
Mischellin <mischellin@outlook.com>
2021-10-04 12:52:32 +0300

    Writ some comments about function and main code in main.py and receiver.py Requests for creating new variables in the database have been expanded and the Time column has been added. And adding database file backup with some examples

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
 import pika
 import random
-import sys
+
 from datetime import datetime
 
 
@@ -9,7 +8,6 @@ def time_gen():
     """
     A function that creates a random date between 03-09-2020 and
     03-09-2021 returning it in the str() type and format: "YYYY-MM-DD, HH:MM:SS"
-    :return:
     """
     timestamp = random.randint(1630683282, 1633275282)
     date_time = datetime.fromtimestamp(timestamp)
@@ -20,7 +18,6 @@ def temperature_gen():
     """
     A function that creates a random temperature between 100 and
     373 degrees Kelvin that returns it as str()
-    :return:
     """
     temper_kel = random.randint(100, 373)
     return str(temper_kel)
@@ -29,18 +26,18 @@ def temperature_gen():
 def pressure_gen():
     """
     A function that creates a random pressure between 10 and 100 Pascal returning it in str() type
-    :return:
     """
     pressure_pa = random.randint(10, 100)
     return str(pressure_pa)
 
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-
-
 def sender(channel, message, connection, event):
-    msg = message+';'+event
+    """
+    In this function, we collect the task sent to the recipient and create it in the queue, with parameters that
+    allow not to lose it if there is no active receiver
+    """
+    msg = message + ';' + event
+    # Creating a complete message to send to the handler along with the event
     channel.queue_declare(queue='SQLLog', durable=True)
     channel.basic_publish(
         exchange='',
@@ -53,26 +50,20 @@ def sender(channel, message, connection, event):
     connection.close()
 
 
-# def main():
 for i in range(10):
     time, temperature, pressure = time_gen(), temperature_gen(), pressure_gen()
+    # Assigning random values to time, temperature and pressure variables
 
     sensor = time + ';' + temperature + ';' + pressure
+    # Creating a single status message from the sensor variables
+
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
+    # Creating a connection to the RabbitMQ task queue server
 
     if int(temperature) > 300:
+        # Checking the Temperature Limit
         sender(channel, sensor, connection, event="Alert")
     else:
         sender(channel, sensor, connection, event="Log")
-
-# if __name__ == '__main__':
-#     try:
-#         main()
-#     except KeyboardInterrupt:
-#         print('Interrupted')
-#         try:
-#             sys.exit(0)
-#         except SystemExit:
-#             os._exit(0)
--- receive.py
+++ receive.py
@@ -3,7 +3,6 @@ import os
 import pika
 import psycopg2
 import sys
-import datetime
 from psycopg2.extras import DictCursor
 from contextlib import closing
 from wpas import *
@@ -14,31 +13,38 @@ def main():
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
-
+    # Connecting to the server from the task queue handler side
     channel.queue_declare(queue='SQLLog', durable=True)
+    # Connecting to the queue
     print(' [*] Waiting for messages. To exit press CTRL+C')
 
     def callback(ch, method, properties, body):
-        #
+        """A function that receives tasks
+        sequentially from the queue"""
         row = body.decode().split(';')
-        # date = row[0].split('|')
-        # date = datetime.time
-
+        # Creating an array from the received message with ';' separator for further processing
         print(row)
-        with closing(
-                psycopg2.connect(dbname='SASTest', user='postgres', password=passwordDB, host='localhost')) as conn:
+        with closing(psycopg2.connect(
+                dbname='SASTest',
+                user='postgres',
+                password=passwordDB,
+                host='localhost')) as conn:
+            # Creating a connection to a postgreSQL database
             with conn.cursor(cursor_factory=DictCursor) as cursor:
                 conn.autocommit = True
-                values = [(row[0], row[1], row[2])]
-                if row[3] != "Alert":
-                    insert = sql.SQL('INSERT INTO logger.log ("date", "temperature", "preassure") VALUES {}').format(
+                values = [(row[0], row[0], row[1], row[2])]
+                if row[3] == "Alert":
+                    insert = sql.SQL('INSERT INTO logger.alert ("date","time","temperature", "preassure") '
+                                     'VALUES {}').format(
                         sql.SQL(',').join(map(sql.Literal, values))
                     )
                 elif row[3] == "Log":
-                    insert = sql.SQL('INSERT INTO log (Date, Temperature, Pressure) VALUES {}').format(
+                    insert = sql.SQL('INSERT INTO logger.log ("date","time","temperature", "preassure") '
+                                     'VALUES {}').format(
                         sql.SQL(',').join(map(sql.Literal, values))
                     )
                 cursor.execute(insert)
+
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
     channel.basic_qos(prefetch_count=1)
